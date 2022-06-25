6af63bde9b66bcb6eb22450da356e6ec403d08cf
dreamsri27 <ss147387@gmail.com>
2022-06-25 23:53:00 +0530

    final code

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
+# from operator import mod
 from django.db import models
 
 # Create your models here.
+class UploadDB(models.Model):
+    date=models.DateTimeField(auto_now_add=True)
+    file=models.FileField(upload_to='media')
+    ulrfile= models.CharField(max_length=100)
+
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+import string,random
+from .models import UploadDB
+from django.http import FileResponse, HttpResponse
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        filename  = request.FILES['filename']
+        n = 7
+        res = ''.join(random.choices(string.ascii_uppercase +string.digits, k = n))
+        urlname = str(res)
+        dbobj = UploadDB(file = filename,ulrfile = urlname)
+        dbobj.save()
+        print(filename)
+        url =  urlname
+        return render(request,"index.html",{'url':url})
+    return render(request,"index.html")
+
+
+def downloadfile(request,slug):
+    dbobj = UploadDB.objects.get(ulrfile= slug)
+    url = dbobj.file.url
+    return render(request,"lastpage.html",{'url':url})
+    
+
+
+
